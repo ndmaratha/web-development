12f7c3b9a5883acbbbc02d47fdf7c66ff76ac305
Nayan Desai <ndmaratha01@gmail.com>
2022-12-09 06:00:52 +0530

    input output done

--- javascript/learning.js
+++ javascript/learning.js
@@ -1,10 +1,13 @@
  var btn_translate= document.querySelector("#btn");
  var txtinput= document.querySelector("#txtinput");
- console.log(txtinput);
- 
+ var outputdiv= document.querySelector("#output");
+/*  console.log(txtinput);
+ */
  function clickeventhandler(){
-     console.log('clicked');
+/*      console.log('clicked');
      console.log("input",txtinput.value);
+ */
+outputdiv.innerText="translated"+txtinput.value;
     }
     
     btn_translate.addEventListener("click", clickeventhandler);
