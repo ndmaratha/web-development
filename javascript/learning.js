00479d462048e16f724301e4d98c6ea23ec757ea
Nayan Desai <ndmaratha01@gmail.com>
2022-12-11 06:03:06 +0530

    js project few left

--- javascript/learning.js
+++ javascript/learning.js
@@ -3,11 +3,28 @@
  var outputdiv= document.querySelector("#output");
 /*  console.log(txtinput);
  */
+var serverurl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function gettranslation(text){
+    return serverurl + "?" + "text=" +text;
+}
+
+function errorhandler(error){
+    console.log("error occured",error);
+    alert("something went wrong try after sometime");
+}
+
  function clickeventhandler(){
 /*      console.log('clicked');
      console.log("input",txtinput.value);
  */
-outputdiv.innerText="translated"+txtinput.value;
-    }
+/* outputdiv.innerText="translated"+txtinput.value; */
+var inputtxt=txtinput.value;
+fetch(gettranslation(inputtxt))
+.then(Response => Response.json())
+.then(json => console.log(json.contents.translated))
+.catch(errorhandler)
+
+};
     
     btn_translate.addEventListener("click", clickeventhandler);
